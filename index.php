dd11bf02dba8e32bc1a0ce841d603d52345f7e28
Ben Ubois <ben@benubois.com>
2011-11-06 10:46:01 -0800

    Added directory listing.

--- index.php
+++ index.php
@@ -1,27 +1,6 @@
-
-<pre>
-	<h2>Memcached</h2>
-	
 <?php
+$dir    = 'tmp';
+$files1 = scandir($dir);
 
-var_export(class_exists('Memcache'));
-
-var_dump($get_result);
-
-$i = 0;
-foreach ($GLOBALS as $global_name => $global_value) 
-{
-	if ($i != 0 && 'i' != $global_name && 'global_value' != $global_name && 'global_name' != $global_name) 
-	{
-		print '<h2>' . $global_name . '</h2>';
-		print '<pre>' . print_r($global_value, TRUE) . '</h2>';
-		
-		error_log(var_export($global_name . ' --------------------------------', TRUE));
-		error_log(var_export($global_value, TRUE));
-		
-	}
-	$i++;
-}
-
+print_r($files1);
 ?>
-</pre>
